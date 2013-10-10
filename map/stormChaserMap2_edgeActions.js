4b126c20735d55c358a95fb964ccfca8d01fc5c6
schneidan <dan@scrollwright.com>
2013-10-09 22:43:46 -0600

    New fully-responsive map from Tom added and testing begun; missing small-grib menu icons fixed.

--- map/stormChaserMap2_edgeActions.js
+++ map/stormChaserMap2_edgeActions.js
@@ -99,6 +99,10 @@ Symbol.bindElementAction(compId,symbolName,"${_pause_btn2}","mouseover",function
 //Edge binding end
 Symbol.bindElementAction(compId,symbolName,"${_play_btn2}","mouseover",function(sym,e){sym.$("playHover_btn2").show();});
 //Edge binding end
+Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){var stageHeight=sym.$("Stage").height();sym.$("#Stage").css({"transform-origin":"0 0","-ms-transform-origin":"0 0","-webkit-transform-origin":"0 0","-moz-transform-origin":"0 0","-o-transform-origin":"0 0"});function scaleStage(){var stage=sym.$("Stage");var parent=sym.$("Stage").parent();var parentWidth=stage.parent().width();var parentHeight=$(window).height();var stageWidth=stage.width();var stageHeight=stage.height();var desiredWidth=Math.round(parentWidth*1);var desiredHeight=Math.round(parentHeight*1);var rescaleWidth=(desiredWidth/stageWidth);var rescaleHeight=(desiredHeight/stageHeight);var rescale=rescaleWidth;if(stageHeight*rescale>desiredHeight)
+rescale=rescaleHeight;stage.css('transform','scale('+rescale+')');stage.css('-o-transform','scale('+rescale+')');stage.css('-ms-transform','scale('+rescale+')');stage.css('-webkit-transform','scale('+rescale+')');stage.css('-moz-transform','scale('+rescale+')');stage.css('-o-transform','scale('+rescale+')');parent.height(stageHeight*rescale);}
+$(window).on('resize',function(){scaleStage();});$(document).ready(function(){scaleStage();});});
+//Edge binding end
 })("stage");
 //Edge symbol end:'stage'
 
--- map/stormChaserMapPart2_edgeActions.js
+++ map/stormChaserMapPart2_edgeActions.js
@@ -83,6 +83,10 @@ Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){
 //Edge binding end
 Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",22192,function(sym,e){sym.stop();});
 //Edge binding end
+Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){var stageHeight=sym.$("Stage").height();sym.$("#Stage").css({"transform-origin":"0 0","-ms-transform-origin":"0 0","-webkit-transform-origin":"0 0","-moz-transform-origin":"0 0","-o-transform-origin":"0 0"});function scaleStage(){var stage=sym.$("Stage");var parent=sym.$("Stage").parent();var parentWidth=stage.parent().width();var parentHeight=$(window).height();var stageWidth=stage.width();var stageHeight=stage.height();var desiredWidth=Math.round(parentWidth*1);var desiredHeight=Math.round(parentHeight*1);var rescaleWidth=(desiredWidth/stageWidth);var rescaleHeight=(desiredHeight/stageHeight);var rescale=rescaleWidth;if(stageHeight*rescale>desiredHeight)
+rescale=rescaleHeight;stage.css('transform','scale('+rescale+')');stage.css('-o-transform','scale('+rescale+')');stage.css('-ms-transform','scale('+rescale+')');stage.css('-webkit-transform','scale('+rescale+')');stage.css('-moz-transform','scale('+rescale+')');stage.css('-o-transform','scale('+rescale+')');parent.height(stageHeight*rescale);}
+$(window).on('resize',function(){scaleStage();});$(document).ready(function(){scaleStage();});});
+//Edge binding end
 })("stage");
 //Edge symbol end:'stage'
 
